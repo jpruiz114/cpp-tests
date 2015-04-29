ec14aa849ba4be455e8868ea25bc4c426db179a4
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-04-28 19:14:33 -0500

    first commit

---
+++ CUDA_Test01/CUDA_Test01/HelloWorld.cu
@@ -0,0 +1,13 @@
+#include "cuda_runtime.h"
+
+#include <stdio.h>
+
+__global__ void mykernel(void) {
+
+}
+
+int main(void) {
+	mykernel<<<1,1>>>();
+	printf("Hello World!\n");
+	return 0;
+}
---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Hello World!" << endl;
+	cout << "Welcome to C++ Programming" << endl;
+	
+	return 0;
+}
